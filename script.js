eab6ee50e6241fb1cab60f56a174c524a3eccffc
anton.korotkyi <antondp750@gmail.com>
2023-09-05 10:15:19 +0300

    Add: script for validating data (login) in login page

---
+++ script.js
@@ -0,0 +1,62 @@
+function showStatus(statusBad, statusNormal, statusGood) {
+    document.querySelector(".status-bad").style.display = statusBad;
+    document.querySelector(".status-normal").style.display = statusNormal;
+    document.querySelector(".status-good").style.display = statusGood;
+}
+
+function validateLogin(login) {
+    let textStatus = document.querySelector(".status-text");
+    const startButton = document.querySelector(".big-purple-button");
+    textStatus.innerHTML = "";
+    let isError = false;
+
+    if (login.length < 3 || login.length > 10) {
+        showStatus("block", "none", "none");
+        textStatus.innerHTML += "Нажаль, помилка - дозволена довжина нікнейму від 3 до 10 символів;<br>";
+        isError = true;
+    }
+
+    if (!/^[0-9А-Яа-яA-Za-zЁёЇїІіЄєҐґ\-'_]+$/.test(login)) {
+        showStatus("block", "none", "none");
+        textStatus.innerHTML += "На жаль, помилка - нікнейм може містити літери (zZ-яЯ),цифри (0-9), спецсимволи (Word space, -, ', _);<br>";
+        isError = true;
+    }
+
+    if (!/^[0-9А-яA-Za-zЁёЇїІіЄєҐґ]/.test(login[0])) {
+        showStatus("block", "none", "none");
+        textStatus.innerHTML += "Нікнейм повинен починатися з літер чи цифр;<br>";
+        isError = true;
+    }
+
+    if (!/^[0-9А-яA-Za-zЁёЇїІіЄєҐґ]/.test(login[login.length-1])) {
+        showStatus("block", "none", "none");
+        textStatus.innerHTML += "Нікнейм повинен закінчуватися на літеру чи цифру;<br>";
+        isError = true;
+    }
+
+    if (!isError) {
+        showStatus("none", "none", "block");
+        textStatus.innerHTML = "";
+        startButton.removeAttribute('disabled');
+    }
+}
+
+window.onload = function (ev) {
+    const startButton = document.querySelector(".big-purple-button");
+    startButton.setAttribute('disabled', '');
+    const nicknameInput = document.querySelector(".login-field");
+    let timeout;
+    showStatus("none", "none", "none");
+
+    nicknameInput.oninput = (event) => {
+        clearTimeout(timeout);
+        timeout = setTimeout(function () {
+                validateLogin(nicknameInput.value.trim());
+            },
+            1000);
+    };
+    nicknameInput.onchange = (click) => {
+        showStatus("none", "block", "none");
+        validateLogin(nicknameInput.value.trim());
+    };
+};
