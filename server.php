b1678d51199eeec0e5fc40888557e70648e68393
anton.korotkyi <antondp750@gmail.com>
2023-09-15 11:59:37 +0300

    Update: add random number status (0-in prepare 1-in search 2-in game)

--- server.php
+++ server.php
@@ -13,12 +13,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $id = rand(0, 100);
     $date = new DateTime();
     $login = $postData['login'];
+    $status = rand(0, 2);
     echo json_encode([
         'messageId' => $id,
         'messageType' => 'isLoginUnique',
         'createDate' => new DateTime(),
         'isUnique' => $unique,
-        'login' => $login
+        'login' => $login,
+        'status' => $status
     ]);
 } else {
     echo "no login";
