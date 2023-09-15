a60d797c8b7d038c320279d42e153ae4c611238d
anton.korotkyi <antondp750@gmail.com>
2023-09-15 17:03:07 +0300

    Update: add connection with server

--- seabattle/acc/connectionWithDB.js
+++ seabattle/acc/connectionWithDB.js
@@ -8,7 +8,16 @@ export function sendSquadron(squadron) {
         const ship = squadron[key];
         const coords = ship.arrDecks.map(deck => String.fromCharCode(97 + deck[1]) + (deck[0] + 1));
         const shipStart = coords[0];
-        const orientation = ship.kx === 1 ? "vertical" : "horizontal";
+        let orientation = "";
+        if (ship.kx === -1) {
+            orientation = "north";
+        } else if (ship.kx === 1) {
+            orientation = "south";
+        } else if (ship.ky === -1) {
+            orientation = "west";
+        } else if (ship.ky === 1) {
+            orientation = "east";
+        }
 
         outputObj[key] = {
             coords,
@@ -19,5 +28,86 @@ export function sendSquadron(squadron) {
     }
 
     localStorage.setItem("shipCoords", JSON.stringify(outputObj));
-    window.location.href = "https://fmc2.avmg.com.ua/study/korotkyi/warship/seabattle/acc/battle";
+    return JSON.stringify(outputObj)
+}
+
+let countdownTimer;
+let timerSeconds = 90;
+export let isTimerRunning = false;
+
+export function startTimer(timerElement, otherElementsToDisable, readyButton) {
+    isTimerRunning = true;
+    disableOtherElements(otherElementsToDisable, true);
+
+    requestToDB({
+        messageId: 9,
+        messageType: "requestIsUsersInQueue",
+        createDate: new Date(),
+        id: Math.random() * 100,
+        firstTurn: Math.random() * 100,
+    }).then(data => {
+        if(data.messageType === "gameCreateInfo" || data.messageType === "gameConnectInfo") {
+            localStorage.setItem("gameInfo", JSON.stringify(data));
+            window.location.href = "https://fmc2.avmg.com.ua/study/korotkyi/warship/seabattle/acc/battle";
+        }
+    });
+
+    countdownTimer = setInterval(function () {
+        timerSeconds--;
+        timerElement.textContent = `${timerSeconds} сек`;
+
+        if (timerSeconds <= 0) {
+            clearInterval(countdownTimer);
+            timerElement.textContent = 'Опонента не знайдено';
+            isTimerRunning = false;
+            disableOtherElements(otherElementsToDisable, false);
+            readyButton.value = 'Готовий';
+        }
+    }, 1000);
+}
+
+export function stopTimer(timerElement, otherElementsToDisable, readyButton) {
+    clearInterval(countdownTimer);
+    isTimerRunning = false;
+    timerSeconds = 90;
+    timerElement.textContent = `${timerSeconds} сек`;
+    disableOtherElements(otherElementsToDisable, false);
+    readyButton.value = 'Готовий';
+}
+
+function disableOtherElements(elements, disable) {
+    elements.forEach(function (element) {
+        if (disable) {
+            element.setAttribute('disabled', '');
+
+        } else {
+            element.removeAttribute('disabled');
+        }
+    });
+}
+
+export function requestToDB(requestData) {
+    const requestBody = JSON.stringify(requestData);
+
+    const url = "https://fmc2.avmg.com.ua/study/korotkyi/warship/seabattle/acc/server.php";
+
+    const requestOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: requestBody
+    };
+
+    return fetch(url, requestOptions)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Ошибка HTTP: " + response.status);
+            }
+            return response.json();
+        })
+        .catch(error => {
+            console.error("Произошла ошибка:", error);
+            throw error;
+        });
 }
