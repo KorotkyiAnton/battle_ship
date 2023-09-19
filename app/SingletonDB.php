947c102d4e87a025a57bccca020433bfb20ea492
anton.korotkyi <antondp750@gmail.com>
2023-09-19 17:00:01 +0300

    Add: add simple MWC back-end and entry point (index.php), add singleton to connect to DB

---
+++ app/SingletonDB.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace app;
+
+use PDO;
+use PDOException;
+
+class SingletonDB
+{
+    private static ?SingletonDB $instance = null;
+    private PDO $pdo;
+
+    private string $host = '10.10.1.133';
+    private string $db = 'study';
+    private string $user = 'a.korotkyi';
+    private string $pass = 'HY&er98f';
+    private string $charset = 'utf8';
+
+    // Private constructor to prevent direct instantiation
+    private function __construct() {
+        try {
+            $dsn = "mysql:host={$this->host};dbname={$this->db};charset={$this->charset}";
+            $this->pdo = new PDO($dsn, $this->user, $this->pass);
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            // Handle database connection error here
+            die("Database connection failed: " . $e->getMessage());
+        }
+    }
+
+    // Get the singleton instance of the Database class
+    public static function getInstance(): ?SingletonDB
+    {
+        if (!self::$instance) {
+            self::$instance = new SingletonDB();
+        }
+        return self::$instance;
+    }
+
+    // Get the PDO connection
+    public function getConnection(): PDO
+    {
+        return $this->pdo;
+    }
+
+    // Prevent cloning of the object
+    private function __clone() {}
+
+    // Prevent serialization of the object
+    private function __wakeup() {}
+}
